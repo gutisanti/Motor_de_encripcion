911479528ec5eff320f97d9e30ee24c5410451ea
EritzSM <140726748+EritzSM@users.noreply.github.com>
2024-02-27 09:00:46 -0500

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,17 @@ class Test(unittest.TestCase):
 
     # Cada prueba unitaria es un metodo la clase
     def testNormal1(self):
+        Entrance = "Hola Mundo"
+        clave = 1234
+        mi_motor = MotorEncriptacion(clave)
+        "mensaje_encriptado = mi_motor.encriptar(Entrance)"
         # Cada metodo de prueba debe llamar un metodo assert
+        # Proceso
+        result = mi_motor.encriptar(Entrance)
+        #Esperado
+        expected = "ԚՁԾԳӲԟՇՀԶՁ"
         # para comprobar si la prueba pasa
-        pass
+        self.assertEqual(expected,result)
 
     def testNormal2(self):
         pass
